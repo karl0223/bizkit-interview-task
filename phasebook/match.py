1f77e6a3d67236b45f227d4a302a2b835db16c59
karl ducta <karlducta022@gmail.com>
2023-12-19 16:56:09 +0800

    Improve the Matching Algorithm

--- phasebook/match.py
+++ phasebook/match.py
@@ -20,8 +20,6 @@ def match(match_id):
 
 
 def is_match(fave_numbers_1, fave_numbers_2):
-    for number in fave_numbers_2:
-        if number not in fave_numbers_1:
-            return False
+    fave_numbers_1_set = set(fave_numbers_1)
 
-    return True
+    return all(number in fave_numbers_1_set for number in fave_numbers_2)
